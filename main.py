f710e59c2878a8b27a797979042789f18ab6f5bd
Maxim Gelfand <gelfandmaxim@gmail.com>
2025-01-10 19:32:45 +0200

    Fix double assignment

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import networkx as nx
 from randomizer import BiasedRandomChoice
 from dub import create_instruction_audio, correct_times, get_instruction_collector_callback
 from random_walk import walk_graph, describe
-from salsa_graph import make_graph, graph_var
+from salsa_graph import make_graph
 
 
 def main():
     graph = nx.MultiDiGraph()
-    graph_var.set(graph)
-
     make_graph(graph)
 
     randomizer = BiasedRandomChoice(graph.nodes, bias_factor=0.0)
